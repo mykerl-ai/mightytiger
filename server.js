816e98c0f7f9020ab52f7d48cbe9acb9c067b70d
mykerl-ai <michaelchigozie51@gmail.com>
2021-10-04 20:31:09 +0100

    express server

--- server.js
+++ server.js
@@ -6,4 +6,15 @@ const port = process.env.PORT || 3000 // Heroku will need the PORT environment v
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(port, () => console.log(`App is live on port ${port}!`))
+app.listen(port, () => console.log(`App is live on port ${port}!`));
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
