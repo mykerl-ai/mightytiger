ed441da10da9d889964507a4f3f5eb6f00cfb76e
mykerl-ai <michaelchigozie51@gmail.com>
2021-10-06 14:20:39 +0100

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import './Navbar.css';
 import {Link} from 'react-router-dom';
+import { useState } from 'react';
 
 const Navbar = () => {
 
+    const [menu, setMenu] = useState(false);
 
     return (
         <header>
@@ -14,6 +16,24 @@ const Navbar = () => {
                 <Link to="/market">For Sale</Link>
                 <Link to="/wallet">My Wallet</Link>
             </div>
+
+            <div onClick={()=> setMenu(!menu)} className="hamburger">
+                <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M25 7.5H12.5" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M25 15H5" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M25 22.5H17.5" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                    {menu &&
+                <div className="hamMenu">
+                    <Link to="/">Home</Link>
+                    <Link to="/gallery">Gallery</Link>
+                    <Link to="/market">For Sale</Link>
+                    <Link to="/wallet">My Wallet</Link>
+                </div>}
+            </div>
+
+            
         </header>
     )
 }
